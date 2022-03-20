6a02435a73ff50bb6ff092d651313516ad1c5cf7
Rachel Gray <rachel.gray7448@gmail.com>
2022-03-19 21:38:45 -0400

    added city name API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
-var lat = 42.6073;
-var lon = 83.9294;
+var cityInputEl = document.querySelector("#cityinput");
 
 
-var getWeather = function() {
+
+
+var getWeather = function(lat, lon, cityname) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely&appid=f40ba3cdc60652f681607937c3b81a09";
     fetch(apiUrl).then(function(response) {
 
@@ -15,6 +16,28 @@ var getWeather = function() {
     })
     }
 
+var getCity = function(cityname){
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q={cityname}&appid=f40ba3cdc60652f681607937c3b81a09";
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                var lat = data.coord.lat;
+                var lon = data.coord.lon;
+                getWeather(lat, lon, cityname)
+            })
+        }
+    })
+}
+
+var cityFinder = function(event) {
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    getCity(city);
+};
+
 // local storage city search history
+
+cityInputEl.addEventListener("submit", cityFinder)
 
 getWeather();
